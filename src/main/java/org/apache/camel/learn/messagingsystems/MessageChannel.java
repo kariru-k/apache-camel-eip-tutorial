1103f00951c1a2ca23dd481a18b54c9ddaa16e50
Keith Kariru Mutuku <karirukeith@students.uonbi.ac.ke>
2023-07-29 17:24:55 +0300

    Edit MessageChannel EIP to send a message

--- src/main/java/org/apache/camel/learn/messagingsystems/MessageChannel.java
+++ src/main/java/org/apache/camel/learn/messagingsystems/MessageChannel.java
@@ -5,8 +5,9 @@ import org.apache.camel.builder.RouteBuilder;
 public class MessageChannel extends RouteBuilder {
 
     @Override
-    public void configure() throws Exception {
-        from("file:src/testdata/input")
+    public void configure(){
+        from("file:src/testdata/input?noop=true")
+                .log("A file!")
                 .to("file:src/testdata/output");
     }
 }
